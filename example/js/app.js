984c080b3663b3f67781a412ef8a2bc61d9901e1
Erik Krogh Kristensen <erik@webbies.dk>
2015-03-03 00:16:23 +0100

    Adding some more directives, and a better example page.

--- example/js/app.js
+++ example/js/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('materializeApp', ['ui.materialize.toast'])
+var app = angular.module('materializeApp', ['ui.materialize'])
+    .controller('BodyController', ["$scope", function ($scope) {
+        $scope.select = {
+            value: "message",
+            choices: ["message", "another message", "I'm am speaking over here!", "Hi there!"]
+        };
+
+        $scope.dummyInputs = {};
+    }]);
