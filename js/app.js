f2a13044709ec811d08a10c43e937606e36822d2
Erik Krogh Kristensen <erik@webbies.dk>
2015-03-29 14:58:47 +0200

    Added short documentation for input-date #10

--- js/app.js
+++ js/app.js
@@ -13,4 +13,33 @@ var app = angular.module('materializeApp', ['ui.materialize'])
         $scope.changePage = function (page) {
             toast("Changed to page " + page, 1000);
         }
+    }])
+    .controller('DateController', ["$scope", function ($scope) {
+        var currentTime = new Date();
+        $scope.currentTime = currentTime;
+        $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        $scope.today = 'Today';
+        $scope.clear = 'Clear';
+        $scope.close = 'Close';
+        $scope.onStart = function () {
+            console.log('onStart');
+        };
+        $scope.onRender = function () {
+            console.log('onRender');
+        };
+        $scope.onOpen = function () {
+            console.log('onOpen');
+        };
+        $scope.onClose = function () {
+            console.log('onClose');
+        };
+        $scope.onSet = function () {
+            console.log('onSet');
+        };
+        $scope.onStop = function () {
+            console.log('onStop');
+        };
     }]);
