1ab1a70b571e307e0a18a18bbcfe3c87acf9b973
Erik Krogh Kristensen <erik@webbies.dk>
2015-07-28 13:03:54 +0200

    Adding documentation for slider directive #44

--- src/angular-materialize.js
+++ src/angular-materialize.js
@@ -24,7 +24,9 @@
     angular.module("ui.materialize.slider", [])
         .directive("slider", ["$timeout", function($timeout){
             return {
+                restrict: 'A',
                 link: function(scope, element, attrs) {
+                    element.addClass("slider");
                     $timeout(function(){
                     	element.slider();
                     });
