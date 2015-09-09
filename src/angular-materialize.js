5a121d607da880acf4e258276d9dd1df80925e3a
Daniel Rodriguez <daniel.rodriguez@buildbinder.com>
2015-09-09 09:05:15 -0600

    updated to last version of angular-materialize

--- src/angular-materialize.js
+++ src/angular-materialize.js
@@ -1,5 +1,5 @@
 (function (angular) {
-    angular.module("ui.materialize", ["ui.materialize.ngModel", "ui.materialize.collapsible", "ui.materialize.toast", "ui.materialize.sidenav", "ui.materialize.material_select", "ui.materialize.dropdown", "ui.materialize.inputfield", "ui.materialize.input_date", "ui.materialize.tabs", "ui.materialize.pagination", "ui.materialize.pushpin", "ui.materialize.parallax","ui.materialize.modal", "ui.materialize.tooltipped",  "ui.materialize.slider", "ui.materialize.materialboxed", "ui.materialize.truncate"]);
+    angular.module("ui.materialize", ["ui.materialize.ngModel", "ui.materialize.collapsible", "ui.materialize.toast", "ui.materialize.sidenav", "ui.materialize.material_select", "ui.materialize.dropdown", "ui.materialize.inputfield", "ui.materialize.input_date", "ui.materialize.tabs", "ui.materialize.pagination", "ui.materialize.pushpin", "ui.materialize.parallax","ui.materialize.modal", "ui.materialize.tooltipped",  "ui.materialize.slider", "ui.materialize.materialboxed"]);
 
     angular.module("ui.materialize.ngModel", [])
         .directive("ngModel",["$timeout", function($timeout){
@@ -87,11 +87,6 @@
                     });
                 }
             };
-        }])
-        .factory('toast', [function(){
-            return function toast(message, time){
-                Materialize.toast(message, time);
-            };
         }]);
 
     angular.module('ui.materialize.pushpin', [])
@@ -174,13 +169,11 @@
 
     /*
      Example usage, notice the empty dropdown tag in the dropdown trigger.
-
      <!-- Dropdown Trigger -->
      <a class='dropdown-button btn' href='javascript:void(0);' data-activates='demoDropdown' 
         dropdown constrain-width="false">
         Select a demo
      </a>
-
      <!-- Dropdown Structure -->
      <ul id='demoDropdown' class='dropdown-content'>
      <li ng-repeat="demo in demoDefiniftions">
@@ -399,6 +392,18 @@
                 return dateFormat(this, mask, utc);
             };
 
+            /**
+             * Validate date object
+             * @param  {Date}  date
+             * @return {Boolean}
+             */
+            var isValidDate = function(date) {
+                if( Object.prototype.toString.call(date) === '[object Date]' ) {
+                    return !isNaN(date.getTime());
+                } 
+                return false;
+            };
+
             return {
                 require: 'ngModel',
                 scope: {
@@ -420,7 +425,9 @@
                     onClose: "&",
                     onSet: "&",
                     onStop: "&",
-                    ngReadonly: "=?"
+                    ngReadonly: "=?",
+                    max: "@",
+                    min: "@"
                 },
                 link: function (scope, element, attrs, ngModelCtrl) {
 
@@ -437,10 +444,11 @@
                         weekdaysFull = (angular.isDefined(scope.weekdaysFull)) ? scope.$eval(scope.weekdaysFull) : undefined,
                         weekdaysLetter = (angular.isDefined(scope.weekdaysLetter)) ? scope.$eval(scope.weekdaysLetter) : undefined;
 
+
                     $compile(element.contents())(scope);
                     if (!(scope.ngReadonly)) {
                         $timeout(function () {
-                            element.pickadate({
+                            var pickadateInput = element.pickadate({
                                 container : (angular.isDefined(scope.container)) ? scope.container : 'body',
                                 format: (angular.isDefined(scope.format)) ? scope.format : undefined,
                                 formatSubmit: (angular.isDefined(scope.formatSubmit)) ? scope.formatSubmit : undefined,
@@ -460,6 +468,22 @@
                                 onSet: (angular.isDefined(scope.onSet)) ? function(){ scope.onSet(); } : undefined,
                                 onStop: (angular.isDefined(scope.onStop)) ? function(){ scope.onStop(); } : undefined
                             });
+                            //pickadate API
+                            var picker = pickadateInput.pickadate('picker');
+
+                            //watcher of min and max
+                            scope.$watch('max', function(newMax) {
+                                if( picker ) {
+                                    var maxDate = new Date(newMax);
+                                    picker.set({max: isValidDate(maxDate) ? maxDate : false});
+                                }
+                            });
+                            scope.$watch('min', function(newMin) {
+                                if( picker ) {
+                                    var minDate = new Date(newMin);
+                                    picker.set({min: isValidDate(minDate) ? minDate : false});
+                                }
+                            });
                         });
                     }
                 }
@@ -474,7 +498,6 @@
         total="100"
         pagination-action="changePage(page)"
         ul-class="customClass">
-
      * ul-class could be either an object or a string
      */
     angular.module("ui.materialize.pagination", [])
@@ -733,19 +756,16 @@
     /*     example usage:
      <!-- Modal Trigger -->
      <a class='btn' href='#demoModal' modal>show Modal</a>
-
      <!-- Modal Structure -->
      <div id="demoModal" class="modal">
      <div class="modal-content">
      <h4>Modal Header</h4>
-
      <p>A bunch of text</p>
      </div>
      <div class="modal-footer">
      <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
      </div>
      </div>
-
      */
     angular.module("ui.materialize.modal", [])
         .directive("modal", ["$compile", "$timeout", function ($compile, $timeout) {
@@ -772,11 +792,9 @@
         
         
     /*     example usage:
-
     <!-- data-position can be : bottom, top, left, or right -->
     <!-- data-delay controls delay before tooltip shows (in milliseconds)-->
     <a class="btn" tooltipped data-position="bottom" data-delay="50" data-tooltip="I am tooltip">Hover me!</a>
-
      */
     angular.module("ui.materialize.tooltipped", [])
         .directive("tooltipped", ["$compile", "$timeout", function ($compile, $timeout) {
@@ -802,7 +820,6 @@
         }]);
 
     /*     example usage:
-
     <!-- normal materialboxed -->
     <img materialboxed class="materialboxed responsive-img" width="650" src="images/sample-1.jpg">
     
@@ -820,28 +837,6 @@
                         element.materialbox();
                     });
                    
-                }
-            };
-        }]);
-
-    angular.module('ui.materialize.truncate', [])
-        .directive('truncate', [function(){
-            // Runs during compile
-            return {
-                // name: '',
-                // priority: 1,
-                // terminal: true,
-                // scope: {}, // {} = isolate, true = child, false/undefined = no change
-                // controller: function($scope, $element, $attrs, $transclude) {},
-                // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-                restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
-                // template: '',
-                // templateUrl: '',
-                // replace: true,
-                // transclude: true,
-                // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-                link: function($scope, iElm, iAttrs, controller) {
-                    iElm.slice(0, iAttrs.limit);    
                 }
             };
         }]);
