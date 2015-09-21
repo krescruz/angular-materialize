80585defb28ec44de6cfd14324f323311416b14d
Erik Krogh Kristensen <erik@webbies.dk>
2015-09-21 15:38:16 +0200

    Triggering keyDown when the model of a textarea has changed, to support Autoresize. fixes #72 Also restricting the input-field stuff to only work on elements that are direct children of the directive element. Also updating jQuery, Angular and Materialize version in index.html Restricting the textarea stuff in input-field to only work on .materialize-textarea And some documentation in index.html to support it.

--- src/angular-materialize.js
+++ src/angular-materialize.js
@@ -256,7 +256,27 @@
                     $timeout(function () {
                         Materialize.updateTextFields();
 
-                        element.find('textarea, input').each(function (index, countable) {
+                        // The "> > [selector]", is to restrict to only those tags that are direct children of the directive element. Otherwise we might hit to many elements with the selectors.
+
+                        // Triggering autoresize of the textareas.
+                        element.find("> > .materialize-textarea").each(function () {
+                            var that = $(this);
+                            that.addClass("materialize-textarea");
+                            that.trigger("autoresize");
+                            var model = that.attr("ng-model");
+                            if (model) {
+                                scope.$parent.$watch(model, function (a, b) {
+                                    if (a !== b) {
+                                        $timeout(function () {
+                                            that.trigger("autoresize");
+                                        });
+                                    }
+                                });
+                            }
+                        });
+
+                        // Adding char-counters.
+                        element.find('> > .materialize-textarea, > > input').each(function (index, countable) {
                             countable = angular.element(countable);
                             if (!countable.siblings('span[class="character-counter"]').length) {
                                 countable.characterCounter();
