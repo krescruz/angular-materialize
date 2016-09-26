98284201627bebf10cb9f9c7427d007de6bef4e1
Erik Krogh Kristensen <erik@webbies.dk>
2016-09-26 23:37:58 +0200

    Making sure the active class gets sets in material-select. Fixes #192

--- src/angular-materialize.js
+++ src/angular-materialize.js
@@ -333,6 +333,18 @@
                             }
 
                             element.siblings(".caret").remove();
+                            function fixActive () {
+                                if (!attrs.multiple) {
+                                    var value = element.val();
+                                    var ul = element.siblings("ul");
+                                    ul.find("li").each(function () {
+                                        var that = $(this);
+                                        if (that.text() === value) {
+                                            that.addClass("active");
+                                        }
+                                    });
+                                }
+                            }
                             scope.$evalAsync(function () {
                                 //element.material_select();
                                 //Lines 301-311 fix Dogfalo/materialize/issues/901 and should be removed and the above uncommented whenever 901 is fixed
@@ -340,6 +352,7 @@
                                     if (!attrs.multiple) {
                                         $('input.select-dropdown').trigger('close');
                                     }
+                                    fixActive();
                                 });
                                 var onMouseDown = function (e) {
                                     // preventing the default still allows the scroll, but blocks the blur.
@@ -349,6 +362,8 @@
                                     }
                                 };
                                 element.siblings('input.select-dropdown').on('mousedown', onMouseDown);
+
+                                fixActive();
                             });
                         }
                         $timeout(initSelect);
